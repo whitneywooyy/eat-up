1d72c954d53a2b6f054b848567b4ec73f7151eb7
Whitney <whitneywooyy@gmail.com>
2015-05-18 13:14:10 -0600

    OAuth

--- server.js
+++ server.js
@@ -1,33 +1,47 @@
-// Dependencies
+// DEPENDENCIES
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
-// INSTALL THE FOLLOWING
 var session = require('express-session');
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// Controllers
+// CONTROLLERS
 
-// Express
+// EXPRESS
 var app = express();
 
 // SERVER VARIABLES
 var port = 8888;
 
 
-// Middleware
+// MIDDLEWARE
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(session({ secret: 'kjhafd9032k23g' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passport
+// requireAuth handles multiple login entrypoints
+var requireAuth = function(req, res, next) {
+  console.log("is authed?", req.user);
+  if (req.isAuthenticated()) {
+  	console.log("You're authed");
+    return next();
+  } else {
+	  console.log('Im redirecting now...');
+	  return res.redirect('/#/login');
+	}	
+}
+app.get('/test', requireAuth, function(req, res) {
+  return res.sendFile(__dirname + '/public/home');
+})
+
+// PASSPORT
 // .serializeUser and .deserializeUser shared between Twitter, Facebook, AND Google
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -47,6 +61,7 @@ passport.use(new TwitterStrategy({
   })
 }));
 passport.use(new FacebookStrategy({
+	// Eat Up App - Facebook Developers
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
     callbackURL: "http://localhost:" + port + "/auth/facebook/callback"
@@ -57,6 +72,7 @@ passport.use(new FacebookStrategy({
   }
 ));
 passport.use(new GoogleStrategy({
+	// Eat Up App - Google Developers Console
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:" + port + "/auth/google/callback"
@@ -66,30 +82,19 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
-var requireAuth = function(req, res, next) {
-  console.log("is authed?", req.user);
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res.redirect('/login');
-}
 
-// Endpoints
-app.get('/test', requireAuth, function(req, res) {
-  return res.status(200).end();
-})
-
+// ENDPOINTS
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/login'
+  successRedirect: '/#/username/dashboard',
+  failureRedirect: '/#/login'
 }), function(req, res) {
 	console.log(req.session);
 });
 app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { 
-	successRedirect: '/dashboard',
-	failureRedirect: '/login' 
+	successRedirect: '/#/username/dashboard',
+	failureRedirect: '/#/login' 
 }), function(req, res) {
 	console.log(req.session);
 });
@@ -97,26 +102,28 @@ app.get('/auth/google', passport.authenticate('google', {
 	scope: ['https://www.googleapis.com/auth/plus.login'] 
 }));
 app.get('/auth/google/callback', passport.authenticate('google', {
-	successRedirect: '/dashboard', 
-	failureRedirect: '/login' 
+	successRedirect: '/#/username/dashboard', 
+	failureRedirect: '/#/login' 
 }), function(req, res) {
-	console.log(req.session);
+	console.log("Response", req.session);
 });
 
 // logout
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
+  console.log("You've logged out");
 });
 
-// Connections
-var mongoUri = 'mongodb://localhost:27017/eat-up';
+// CONNECTIONS
+var mongoUri = 'mongodb://localhost:27017/eatup';
 
 mongoose.connect(mongoUri);
 mongoose.connection.once('open', function() {
   console.log('Connected to MongoDB at ', mongoUri);
 });
 
+// LISTENING
 app.listen(port, function() {
   console.log('Listening on port ', port);
 });
